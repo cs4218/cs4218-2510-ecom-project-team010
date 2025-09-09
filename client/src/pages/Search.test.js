890f099a4a9e4c4106b011785d9ac4b44ea96996
yleeyilin <yiiiliiinn@gmail.com>
2025-09-10 01:49:47 +0800

    Fix warnings

--- client/src/pages/Search.test.js
+++ client/src/pages/Search.test.js
@@ -6,6 +6,13 @@ jest.mock("../components/Layout", () => ({ children }) => (
   <div data-testid="layout">{children}</div>
 ));
 
+jest.mock("../components/Layout", () => {
+  return {
+    __esModule: true, 
+    default: ({ children }) => <div data-testid="layout">{children}</div>,
+  };
+});
+
 jest.mock("../context/search", () => ({
   useSearch: jest.fn(),
 }));
@@ -17,7 +24,7 @@ describe("Given that multiple products are available in the Home Page", () => {
     jest.clearAllMocks();
   });
 
-  it("When search results is empty", () => {
+  test("When search results is empty", () => {
     useSearch.mockReturnValue([{ results: [] }, jest.fn()]);
 
     render(<Search />);
@@ -26,7 +33,17 @@ describe("Given that multiple products are available in the Home Page", () => {
     expect(screen.getByText("No Products Found")).toBeInTheDocument();
   });
 
-  it("When multiple results are present", () => {
+  test("When a single results is present", () => {
+    const mockProduct = [{ _id: "1", name: "Product A", description: "Desc", price: 10 }];
+    useSearch.mockReturnValue([{ results: mockProduct }, jest.fn()]);
+
+    render(<Search />);
+
+    expect(screen.getByText("Found 1")).toBeInTheDocument();
+    expect(screen.getByText("Product A")).toBeInTheDocument();
+    });
+
+  test("When multiple results are present", () => {
     const mockProducts = [
       { _id: "1", name: "Product A", description: "Mock description of product A", price: 10 },
       { _id: "2", name: "Product B", description: "Mock description of product B", price: 20 },
