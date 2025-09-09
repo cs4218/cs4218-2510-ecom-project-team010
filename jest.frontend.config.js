7369db9319de6b7f753a84be3df28610ea25c43a
yleeyilin <yiiiliiinn@gmail.com>
2025-09-10 02:05:45 +0800

    Fix test config

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -21,13 +21,15 @@ export default {
 
   // only run these tests
   testMatch: [
-    "<rootDir>/client/src/pages/Auth/*.test.js",
     "<rootDir>/client/src/pages/**/*.test.js"
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: [
+    "client/src/pages/Auth/**",
+    "client/src/pages/Search.js",
+  ],
   coverageThreshold: {
     global: {
       lines: 100,
